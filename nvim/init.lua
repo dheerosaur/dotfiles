8a4c9845de4da3b1095f62dfad2cfc0caecc2924
Dheeraj Sayala <dheerosaur@gmail.com>
2023-08-28 12:53:28 -0400

    rename help to vimdoc

--- nvim/init.lua
+++ nvim/init.lua
@@ -284,7 +284,7 @@ vim.treesitter.language.register('markdown', 'mdx')
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vimdoc' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
